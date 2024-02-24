9e01d6d6b013636e618e34dbbbf1a396c14f2105
sclie001 <daisysc7789@gmail.com>
2024-02-24 17:58:41 -0500

    created function to generate random number between range given

--- guessing_game/main.py
+++ guessing_game/main.py
@@ -5,3 +5,12 @@ import math
 lower_bound = int(input('Enter lower bound of range:- '))
 
 upper_bound = int(input('Enter upper bound of range:- '))
+
+
+
+#generate random number
+def generateRandomNum():
+    random_number = random.randint(lower_bound, upper_bound)
+    return random_number
+
+print(generateRandomNum())
