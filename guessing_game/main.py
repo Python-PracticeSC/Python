a9bf92b273da64026655a60b0083ba699c66e3f5
sclie001 <daisysc7789@gmail.com>
2024-02-24 19:37:44 -0500

    take in player's guess

--- guessing_game/main.py
+++ guessing_game/main.py
@@ -6,8 +6,8 @@ import math
 lower_bound = int(input('Enter lower bound of range:- '))
 upper_bound = int(input('Enter upper bound of range:- '))
 
-count = 0 '''count number of guesses made'''
-MAX_GUESSES = 6 '''Maximum number of guesses'''
+count = 0 #count number of guesses made
+MAX_GUESSES = 6 #Maximum number of guesses
 
 
 #generate random number
@@ -15,4 +15,11 @@ def generateRandomNum():
     random_number = random.randint(lower_bound, upper_bound)
     return random_number
 
-print(generateRandomNum())
+#check guess against random number
+def checkGuesses(random_number):
+    guess = int(input('Guess a number:- '))
+
+        
+
+random_number = generateRandomNum()
+checkGuesses(random_number)
